8745bda224dc7e00d01538bf7f92dde2c572d283
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-11-24 10:51:45 -0600

    Parte móvil del inicio de sesión mediante un servicio remoto

--- app/src/main/java/mx/edu/uteq/dapps/demonavdrawer196/pub/LoginFragment.java
+++ app/src/main/java/mx/edu/uteq/dapps/demonavdrawer196/pub/LoginFragment.java
@@ -1,5 +1,6 @@
 package mx.edu.uteq.dapps.demonavdrawer196.pub;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,6 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
 
 import mx.edu.uteq.dapps.demonavdrawer196.MainActivity;
 import mx.edu.uteq.dapps.demonavdrawer196.R;
@@ -21,6 +36,15 @@ public class LoginFragment extends Fragment {
      */
     private FragmentLoginBinding binding;
 
+    /*
+    Instancia de Volley
+    Instancia de petición al servidor
+    ProgressDailog
+     */
+    private RequestQueue conexionServ;
+    private StringRequest peticionServ;
+    private ProgressDialog progress;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -30,18 +54,71 @@ public class LoginFragment extends Fragment {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        conexionServ = Volley.newRequestQueue(getActivity());
+
+        progress = new ProgressDialog(getActivity());
+        progress.setTitle("Autenticando");
+        progress.setMessage("Por favor espere...");
+        progress.setCancelable(false);
+        progress.setIndeterminate(true);
+
         /*
         Programamos el click de login
          */
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(
-                        new Intent(
-                                getActivity(),
-                                MainActivity.class
-                        )
-                );
+
+                //Creamos valores finales (constantes) con el usuario y contrseña
+                final String usuario = binding.tietUsuario.getText().toString();
+                final String password = binding.tietPassword.getText().toString();
+
+                /*
+                Te falta validar
+                 */
+                progress.show();
+
+                peticionServ = new StringRequest(
+                        //1 Tipo de envio
+                        Request.Method.POST,
+                        //2 Url del servicio
+                        "https://zavaletazea.dev/labs/awos-dapps196/api/auth/login",
+                        //3 Codigo cuando todo sale bien
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                progress.hide();
+                                Toast.makeText(getActivity(), response,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        //4 Codigo cuando existen errores
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                progress.hide();
+                                Toast.makeText(getActivity(), error.toString(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                //Agregamos las variables post
+                ){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        //Creamos un mapa para agregar los nombres de las variables
+                        //y sus valores
+                        Map<String, String> parametros = new HashMap<>();
+
+                        //Agregamos cada nombre de variable y su valor
+                        parametros.put("usuario", usuario);
+                        parametros.put("password", md5(password));
+
+                        //Retornamos los parámetros del servicio
+                        return parametros;
+                    }
+                };
+                //Agregamos la petición al servidor
+                conexionServ.add(peticionServ);
             }
         });
 
@@ -55,6 +132,24 @@ public class LoginFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public String md5(String texto) {
+        //Varuable para generar un hash de md5
+        MessageDigest m = null;
+        try {
+            //Invocamos a md5
+            m = MessageDigest.getInstance("MD5");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        //creamos una semilla random de md5
+        m.update(texto.getBytes(), 0, texto.length());
+        String textoEncriptrado = new BigInteger(1, m.digest()).toString();
+
+        return textoEncriptrado;
     }
 
 }
